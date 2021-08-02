bc72bb23cbd9711aa86e8597f3e438d19c0d1382
Derimar Gray <charminvidal@gmail.com>
2021-08-01 19:24:18 -0500

    next questions and timer working

--- resources/js/script.js
+++ resources/js/script.js
@@ -11,7 +11,7 @@ var answerBtn = document.getElementById("answers");
 var verifyAnswer = document.getElementById("verify-answer");
 var nextBtn = document.getElementById("next-button");
 
-var questionsBody = [
+var questions = [
     {
         question: "Commonly used data types Do Not Include:",
         answers: [
@@ -67,33 +67,36 @@ nextBtn.addEventListener("click", () => {
     nextQuestion();
 });
 
-// start timer
-function timeStart() {
-    timeleft--;
+
+// Count timer
+function Count() {
+    timeLeft--;
     timerCount.textContent = "Time: " + timeLeft;
     if (timeLeft <= 0) {
-        // score();
+        Score();
     }
 };
 
-// start quiz
+
+// Start Quiz
 function startQuiz() {
-    timerID = setInterval(timeStart, 1000);
+    timerID = setInterval(Count, 1000);
     startBlock.classList.add("hidded");
     randomQuestion = questions.sort(() => Math.random() - .5)
     questionIndex = 0
     questionBlock.classList.remove("hidded");
 
-    // clicking button timer will start
-    timeStart();
+    Count();
     nextQuestion();
 };
 
+
 // next question
 function nextQuestion() {
-    clearStatus();
+    resetStatus();
     showQuestion(randomQuestion[questionIndex]);
 };
+
 
 // show questions
 function showQuestion(question) {
@@ -105,16 +108,84 @@ function showQuestion(question) {
         if (answer.correct) {
             button.dataset.correct = answer.correct
         }
-        button.addEventListener("click", selectAnswer)
+        button.addEventListener("click", chooAnswer)
         answerBtn.appendChild(button)
     })
 };
 
-// clear status
-function clearStatus() {
+
+// Reset state function
+function resetStatus() {
+    //clearStatusClass(document.body)
     nextBtn.classList.add("hidded")
     verifyAnswer.classList.add("hidded")
     while (answerBtn.firstChild) {
-        answerBtn.classList.removeChild(answerBtn.firstChild)
+        answerBtn.removeChild
+            (answerBtn.firstChild)
     }
+};
+
+
+// Select answer function
+function chooAnswer(e) {
+    var selected = e.target;
+    //console.dir(selectedButton);
+    var correct = selected.dataset.correct;
+    verifyAnswer.classList.remove("hidded")
+    
+    // Check if the answer correct or wrong then show text
+    if (correct) {
+        verifyAnswer.innerHTML = "Correct!";
+    } else {
+        verifyAnswer.innerHTML = "Wrong!";
+        if (timeLeft <= 10) {
+            timeLeft = 0;
+        } else {
+            // If the aswer is wrong, deduct time by 10
+            timeLeft -= 10;
+        }
+    }
+
+    Array.from(answerBtn.children).forEach(button => {
+        setClass(button, button.dataset.correct)
+    })
+
+    if (randomQuestion.length > questionIndex + 1) {
+        nextBtn.classList.remove("hidded")
+        verifyAnswer.classList.remove("hidded")
+    } else {
+        startBtn.classList.remove("hidded")
+        Score();
+    }
+};
+
+
+// verify selected answer and give color
+function setClass(element, correct) {
+    clearClass(element)
+    if (correct) {
+        element.classList.add("good");
+    } else {
+        element.classList.add("bad");
+    }
+};
+
+
+// Remove classes
+function clearClass(element) {
+    element.classList.remove("good");
+    element.classList.remove("bad");
+};
+
+
+// Save scores
+function Score() {
+    clearInterval(timerID);
+    timerCount.textContent = "Time: " + timeLeft;
+    setTimeout(function () {
+        questionBlock.classList.add("hidded");
+        document.getElementById("score-block").classList.remove("hidded");
+        document.getElementById("your-score").textContent = "Your final score is " + timeLeft;
+
+    }, 2000)
 };
